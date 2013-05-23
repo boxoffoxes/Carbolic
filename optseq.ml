00fa306eee77109396a6e16068220e9f32a62fd1
G Faulkner <glyn.faulkner@york.ac.uk>
2013-05-23 14:11:38 +0100

    Added top-level makefile to build ocaml compiler.

--- optseq.ml
+++ optseq.ml
@@ -1,6 +1,20 @@
 open Str 
 open Unix
 
+type settings = {
+    src:string ;
+    bin:string ;
+    opt_src:string ;
+    opt_cmd:string array ;
+    compile_cmd:string array ;
+}
+
+
+let default_opt_src = "optimised.ll"
+let default_opt_cmd = [| "/usr/bin/opt" ; "-S" ; "-o" ; optimised_src |]
+
+let default_bin = "a.out"
+let compile_cmd = [| "" ; "-o" ; binary |]
 
 (** {1 Utility functions} *)
 
@@ -19,7 +33,7 @@ let read_file_to_list filename =
 
 
 let run_child_process args =
-    (* TODO: Don't forget that cmd needs to be duplicated as args.(0) for execv! *)
+    (* Get the name of the executable from the first arg *)
     let cmd = args.(0) in
     let pid = fork () in
     match pid with
@@ -28,29 +42,86 @@ let run_child_process args =
     | _ -> ignore ( wait () ) ;
 ;;
 
+let show_commandline a =
+    let print_elem = function word -> print_string word ; print_string " " ; in
+    Array.iter print_elem a ;
+    (* ignore ( Array.map Digest.string a ) ; *)
+    print_newline () ;
+;;
+
+let checksum file =
+    Digest.file file
+;;
+
 
 (** {1 Generating random option lists} *)
 
-let extract_opts l =
+let load_optlib filename =
+    let lines = read_file_to_list filename in
     let rex = Str.regexp " +" in
     let first_word str = List.hd ( Str.split rex str ) in
-    Array.of_list ( List.map first_word l )
+    Array.of_list ( List.map first_word lines )
 ;;
 
-let random_opt opts =
-    let sz = Array.length opts in
+let random_opt optlib =
+    let sz = Array.length optlib in
     let i = Random.int sz in
-    opts.(i)
+    optlib.(i)
 ;;
 
-let rec n_random_opts n opts =
-    match n with 
-    | 0 -> []
-    | _ -> ( random_opt opts ) :: (n_random_opts (n - 1) opts)
+let rec n_random_opts n optlib =
+    let ropts = Array.make n "" in
+    for i=0 to n-1 do
+        ropts.(i) <- random_opt optlib
+    done ;
+    ropts
 ;;
+
+(** {1 Generating optimised binaries} *)
+
+let optimise opts =
+    let cmdline = Array.concat [ opt_cmd ; opts ] in
+    run_child_process cmdline ;
+    checksum optimised_src
+;;
+
+let compile () =
+    let cmdline = compile_cmd in
+    run_child_process cmdline ;
+    checksum binary
+;;
+
+
+(** {1 Running simulations} *)
+
+(** Run @param n simulations with @param static_opts and @param k random
+ * optimisations *)
+let rec run_simulation optlib n k static_opts = match n with
+    | 0 -> ()
+    | _ -> (
+        let ropts = n_random_opts k optlib in
+        let cmdline = Array.concat [ opt_cmd ; [| static_opts |] ; ropts ] in
+        show_commandline cmdline ;
+        run_simulation optlib (n-1) k static_opts ;
+    )
+;;
+
+(** Run @param n simulations, using @param k randomly selected optimisations pulled from
+ * @param optlib, and including @param static_opts at the head of the list of
+ * optimisations for every simulation. *)
+let run_simulations optlib static_opts k n =
+    let sim = run_simulation optlib n k in
+    Array.iter sim optlib ;
+;;
+
+
 
 
 (** {1 Benchmarking compiled executables} *)
+
+(* TODO: don't bother executing programs with the same MD5 sum as previously
+ * benchmarked programs as they will have identical performance characteristics.
+ * Should offer a performance hike, particularly for simple programs. *)
 
 let benchmark_prog args =
     let a = fork () in
@@ -70,20 +141,25 @@ let benchmark_prog args =
 ;;
 
 
-(** {1 Program entry point} *)
+(** {1 Program entry point and commandline parsing} *)
+
+let parse_args () =
+    
+;;
 
 let main () =
-    let lines = read_file_to_list "Doc/all-opts.txt" in
-    let opts = extract_opts lines in
+    let (optlib, ll_src) = parse_args  in
+    let cksums = Hashtbl.create 1000 in
     Random.self_init () ;
-    ignore ( List.map print_endline ( n_random_opts 25 opts ) ) ;
-    print_newline ();
 
     benchmark_prog [| "/bin/ls" ; "-al" |] ;
+
+    run_simulations optlib [| "-lint" ; "-roller" |]  10 100 ;
 ;;
 
 (* let opts = extract_opts <<- read_file_to_list "Doc/all-opts.txt" ;;
 
  ;; *)
+
 
 main ()
