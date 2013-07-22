467320994d8ecada1267ef2c45e159a3e94e7cd3
G Faulkner <glyn.faulkner@york.ac.uk>
2013-07-22 20:54:31 +0100

    Suffering bizarre bug where ghc ceases to be executable mid-way through compilation. Switched from make to direct ghc call.

--- carbolic.ml
+++ carbolic.ml
@@ -68,7 +68,8 @@ let prepare dir =
 
 let compile opts =
     let optstr = String.concat " -optlo" (List.rev opts) in
-    let cmd = String.concat "" [ "make " ; "PASSES=\"-optlo" ; optstr ; "\"" ] in
+    let cmd = String.concat "" [ "ghc -O0 -o a.out -fllvm -fforce-recomp -optlo" ; optstr ; " countdown.hs" ] in
+    (* let cmd = String.concat "" [ "make " ; "PASSES=\"-optlo" ; optstr ; "\"" ] in *)
     let logfile = open_out "carbolic.log" in
     output_string logfile optstr ;
     output_string logfile "\n" ;
